6cfe9422ad79f000886d408ac76a4a998250f3b7
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-04 17:56:32 -0600

    modificacion de views en funciones

--- dj_puro/views.py
+++ dj_puro/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import JsonResponse
 from .models import Category
 # Create your views here.
 
 def categoria_list(request):
-    pass
+    MAX_OBJECTS = 20
+    categorys = Category.objects.all()[:MAX_OBJECTS]
+    data = {
+        'res' : list(categorys.values('description','active'))
+    }
+    return JsonResponse(data);
 
 def categoria_details(request, id):
-    pass
+    category = get_object_or_404(Category, pk=id)
+    data = {
+        "result": {
+            'description' :category.description,
+            'active': category.active
+        }
+    }
+    category = Category.objects.get(id = id)
+    return JsonResponse(data) 
