4a944aa00b1d826cc4e632243afa603b390f35fa
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-05 15:24:34 -0600

    se agrego la fucion owner de django

--- api/models.py
+++ api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class OwnerModel(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+
 
 class Categoria(models.Model):
     descripcion = models.CharField(
