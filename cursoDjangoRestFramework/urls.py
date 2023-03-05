f3fd12420cab023d92df4e1d60218f263f590b57
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-04 18:10:10 -0600

    modificacion de url hacia falta un /

--- cursoDjangoRestFramework/urls.py
+++ cursoDjangoRestFramework/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dj-puro', include('dj_puro.urls'))
+    path('dj-puro/', include('dj_puro.urls'))
 ]
