3d4f3a08e53ef0e440d6f880951968b0a4bb1f62
the Richie <richie.lugo.recillas.1990@gmail.com>
2023-03-04 23:09:48 -0600

    simplificando vistas genericas de restframework

--- api/apiviews.py
+++ api/apiviews.py
@@ -1,18 +1,27 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+#from rest_framework.views import APIView
+#from rest_framework.response import Response
+#from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Producto
 from .serializers import ProductoSerializer
 
-class ProductoList(APIView):
-    def get(self, request):
-        prod = Producto.objects.all()
-        data = ProductoSerializer(prod, many= True).data
-        return Response(data)
+#class ProductoList(APIView):
+#    def get(self, request):
+#        prod = Producto.objects.all()
+#        data = ProductoSerializer(prod, many= True).data
+#        return Response(data)
 
-class ProductoDetalle(APIView):
-    def get(self, request,pk):
-        prod = get_object_or_404(Producto,pk=pk)
-        data = ProductoSerializer(prod).data
-        return Response(data)
+#class ProductoDetalle(APIView):
+#    def get(self, request,pk):
+#        prod = get_object_or_404(Producto,pk=pk)
+#        data = ProductoSerializer(prod).data
+#        return Response(data)
+
+class ProductoList(generics.ListCreateAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+class ProductoDetalle(generics.RetrieveDestroyAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
